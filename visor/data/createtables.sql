a6bf29f287b88e83ccbe811de52836bb03af1547
Dave Jensen <dave.jensen.55@gmail.com>
2020-04-03 14:12:19 +0000

    add output files to createtables.sql

--- visor/data/createtables.sql
+++ visor/data/createtables.sql
@@ -1,6 +1,9 @@
-
 DROP TABLE onhandmaterial;
 DROP TABLE printer;
+DROP TABLE requestQuantity;
+DROP TABLE shipQuantity;
+DROP TABLE subVol;
+DROP TABLE prodVol;
 create table printer (
     "name"  VARCHAR, 
     "location" VARCHAR,
@@ -24,10 +27,36 @@ create table onHandMaterial (
 );
 create table requestQuantity (
     "location" VARCHAR,
-    "date" DATE,
+    "date" INTEGER CHECK (date >=0 ),
     "requestedQuantity" FLOAT,
     PRIMARY KEY("location", "date" )
 );
-
+create table shipQuantity (
+    "requestLocation" VARCHAR,
+    "producingLocation" VARCHAR,
+    "date" DATE,
+    "rhipQuantity" FLOAT,
+    PRIMARY KEY("requestLocation", "producingLocation", "date" )
+);
+create table subVol (
+    "printerName" VARCHAR,
+    "printerLocation" VARCHAR,
+    "materialLocation" VARCHAR,
+    "filamentSize" VARCHAR CHECK("filamentSize" = '175mm' or "filamentSize" = '285mm'),
+    "plasticType" VARCHAR CHECK("plasticType" = 'PETG' or "plasticType" = 'PLA' or "plasticType" = 'ABS' or "plasticType" = 'ONYX'),    
+    "period" INTEGER,
+    "quantity" FLOAT,
+    "own"  BOOLEAN,
+    PRIMARY KEY("printerName", "printerLocation", "materialLocation", "filamentSize",  "plasticType", "period", "own")
+);
+create table prodVol (
+    "printerName" VARCHAR,
+    "printerLocation" VARCHAR,
+    "materialLocation" VARCHAR,
+    "period" INTEGER,
+    "quantity" FLOAT,
+    "own"  BOOLEAN,
+    PRIMARY KEY("printerName", "printerLocation", "period")
+);
 
 INSERT INTO onHandMaterial (location, "filamentSize", "plasticType", quantity, share) VALUES ('a','175mm', 'PETG', 0.5, 38);
