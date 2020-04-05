ca3f84a1012cd67b257c12902d759d9a66269a3e
dlj <dlj@cryptonumerics.com>
2020-04-05 16:34:37 -0600

    moved to key annotation in html; enabled material and quntityy tabs

--- visor/web/static/main.js
+++ visor/web/static/main.js
@@ -2,23 +2,35 @@
     function processform(e) {
    var payload={}
         $('.form-group',$(e.target).parent()).each(function(itag,tag) {
-            var input=$("input",tag)
-             var value = null
-             var fieldname = input.attr('data-fieldname')
-             if(input.hasClass("form-check-input")) {
-                 value = input.prop("checked")
-             }
-             else {
-                 value=input.val()
-             }
-             payload[fieldname]=value
-             return;
-         }
-                                                   )
+            var inputs=$("input",tag)
+            inputs.each(function(itag,tag){
+                var input = $(tag)
+                var value = null
+                var fieldname = input.attr('data-fieldname')
+                var keyfield=input.attr('data-key')?(input.attr('data-key')=="true"):false
+                if(input.attr("type")=="checkbox") {
+                    value = input.prop("checked")
+                    payload[fieldname]={value: value, keyfield:keyfield}
+                }
+                else if(input.attr("type")=="radio") {
+                    if(input.prop("checked")) {
+                        fieldname=input.attr("name")
+                        value=input.attr('data-value')
+                        payload[fieldname]={value: value, keyfield:keyfield}
+                    }
+                }
+                else {
+                    value=input.val()
+                    payload[fieldname]={value: value, keyfield:keyfield}
+                }
+            })
+            return
+        })
         return payload
     }
      function post(e, verb, action) {
          var payload=processform(e)
+         payload={json: JSON.stringify(payload)}
          $.post("/visor/"+verb+"?action="+action, payload).done(function(data) {
              $("#status").text(data.sql)
              $("#stdout").text(data.stdout)
@@ -35,42 +47,49 @@
          e.preventDefault()
      })
      $("#updateprinter").click(function(e) {
-         // alert('submit pressed')
-         var payload=processform(e)
-         $.post("/visor/printer?action=update", payload).done(function(data) {
-             $("#status").text(data.sql)
-             $("#stdout").text(data.stdout)
-             $("#stderr").text(data.stderr)
-             return;
-         }).fail(function(data) {
-             return;
-         })
+         // alert('submit
+         post(e,"printer","update")
          e.preventDefault()
      })
      $("#deleteprinter").click(function(e) {
          // alert('submit pressed')
-         var payload=processform(e)
-         $.post("/visor/printer?action=delete", payload).done(function(data) {
-             $("#status").text(data.sql)
-             $("#stdout").text(data.stdout)
-             $("#stderr").text(data.stderr)
-             return;
-         }).fail(function(data) {
-             return;
-         })
+         post(e,"printer","delete")
          e.preventDefault()
      })
-     $("#submitmaterial").click(function(e) {
-         alert('submit pressed')
+     $("#addmaterial").click(function(e) {
+         // alert('submit pressed')
+         post(e,"material","add")
          e.preventDefault()
      })
-     $("#submitrequest").click(function(e) {
-         alert('submit pressed')
+     $("#updatematerial").click(function(e) {
+         // alert('submit
+         post(e,"material","update")
+         e.preventDefault()
+     })
+     $("#deletematerial").click(function(e) {
+         // alert('submit pressed')
+         post(e,"material","delete")
+         e.preventDefault()
+     })
+     $("#addrequest").click(function(e) {
+         // alert('submit pressed')
+         post(e,"request","add")
+         e.preventDefault()
+     })
+     $("#updaterequest").click(function(e) {
+         // alert('submit
+         post(e,"request","update")
+         e.preventDefault()
+     })
+     $("#deleterequest").click(function(e) {
+         // alert('submit pressed')
+         post(e,"request","delete")
          e.preventDefault()
      })
      $("#submitsql").click(function(e) {
          // alert('submit pressed')
          var payload=processform(e)
+         payload={json: JSON.stringify(payload)}
          $.post("/visor/sql", payload).done(function(data) {
              $("#status").text(data.sql)
              $("#stdout").text(data.stdout)
@@ -81,10 +100,11 @@
          })
          e.preventDefault()
      })
+
      $('.nav-tabs a').on('shown.bs.tab', function(event){
          var x = $(event.target).text();         // active tab
          var y = $(event.relatedTarget).text();  // previous tab
-         $("#status").text("Changing tabs")
+         $("#status").text(`Changing tabs: ${y} => ${x}`)
          $("#stdout").text("")
          $("#stderr").text("")
      });
